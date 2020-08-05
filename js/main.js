3f60abec2fb903c5b384838618d2d8a99dbdce29
sam98brown <sam98brown@gmail.com>
2020-08-05 13:39:53 +0100

    Added featured post js slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 //Hamburger icon functionality
 
 // Look for .hamburger
@@ -15,7 +17,18 @@
 var slideout = new Slideout({
   'panel': document.getElementById('panel'),
   'menu': document.getElementById('mobile-menu'),
-  'padding': 256,
+  'padding': 300,
   'tolerance': 70,
   'side': 'right'
+});
+
+  $('#featured nav ul li button').click(function(){
+    if(!$(this).parent('li').hasClass('active')) {
+      $('#featured nav ul li').removeClass('active')
+      $(this).parent('li').addClass('active')
+      const postId = $(this).text()
+      $(`#featured .featured-post`).hide()
+      $('#featured .post-id-'+postId).show()
+    }
+  });
 });
